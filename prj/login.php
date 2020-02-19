86907b97f5f0e5ceaa2693cd1057c94449244aa6
Doomy7 <g.xeirmpos@gmail.com>
2020-02-19 12:18:00 +0200

    Registration Ready

---
+++ prj/login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/form.css">
+    <title>Login</title>
+</head>
+<body>
+    <div class="container">
+
+        <div class="form">
+            <h2>Log In!</h2>
+            <form >
+            <input type="text"  >
+            <input type="text" >
+            <input type="submit" value="Log In">
+            <?php
+              if(isset($_GET['error'])){
+                $error = $_GET['error'];
+                $file = fopen("php/includes/errors.txt","r");
+                while($row = fgets($file)) {
+                  list( $erNo, $type ) = explode( ",", $row );
+                  if($erNo == $error){?>
+                    <input type="text" name="" disabled value="<?php echo "ERROR: ".$type; ?>">
+                  <?php
+                  }
+                }
+                fclose($file);
+              }
+
+             ?>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
--- prj/php/register.php
+++ prj/php/register.php
@@ -1,87 +1,123 @@
 <?php
 
+  include_once "includes/dbinc.php";
 
   if(isset($_POST['submit'])){
 
-    include_once "includes/dbinc.php";
     $name = mysqli_real_escape_string($conn, $_POST['name']);
     $surn = mysqli_real_escape_string($conn, $_POST['surname']);
     $un = mysqli_real_escape_string($conn, $_POST['username']);
     $email = mysqli_real_escape_string($conn, $_POST['email']);
-    $pass1 = mysqli_real_escape_string($conn, $_POST['password']);
-    $pass2 = mysqli_real_escape_string($conn, $_POST['password2']);
+    $pass1 = mysqli_real_escape_string($conn, $_POST['pass1']);
+    $pass2 = mysqli_real_escape_string($conn, $_POST['pass2']);
     $id = mysqli_real_escape_string($conn, $_POST['type']);
 
     if(empty($name) || empty($surn) || empty($un) || empty($email) || empty($pass1) || empty($pass2) || $id == "none"){
-      header("Location: ../signup.php?error=0");
+      header("Location: ../signup.php?error=00");
       exit();
     }else if ($pass1 !== $pass2){
-      header("Location: register.php?error=3");
+      header("Location: ../signup.php?error=01");
       exit();
     }else if(!preg_match("/^[a-zA-Z0-9]*$/", $un)){
-      header("Location: register.php?error=5");
+      header("Location: ../signup.php?error=02");
       exit();
     }else if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-      header("Location: register.php?error=4");
-      exit();
-    }else if(!filter_var($passrem, FILTER_SANITIZE_STRING)){
-      header("Location: register.php?error=8");
+      header("Location: ../signup.php?error=03");
       exit();
     }else{
+      check_email("students", $id, $conn);
+      check_email("teachers", $id, $conn);
+      check_username("students", $id, $conn);
+      check_username("teachers", $id, $conn);
+    }
+    insert($name, $surn, $email, $pass1, $un, $id, $conn);
+  }
 
-      //email check
-      $stmt1 = mysqli_stmt_init($conn);
-      $sqlq1 = "SELECT * FROM `users` WHERE `email` = ?";
-      if(!mysqli_stmt_prepare($stmt1, $sqlq1)){
-        echo 'SQL ERROR';
-      }else{
-        mysqli_stmt_bind_param($stmt1, "s", $email);
-        mysqli_stmt_execute($stmt1);
-        mysqli_stmt_store_result($stmt1);
-        $result_check = mysqli_stmt_num_rows($stmt1);
-        if($result_check>0){
-          header("Location: register.php?error=1");
-          exit();
-        }
-      }
-
-      //userName check
-      $sqlq2 = "SELECT * FROM `users` WHERE `userName` = ?";
-      $stmt2 = mysqli_stmt_init($conn);
-      if(!mysqli_stmt_prepare($stmt2, $sqlq2)){
-        echo 'SQL ERROR';
-      }else{
-        mysqli_stmt_bind_param($stmt2, "s", $un);
-        mysqli_stmt_execute($stmt2);
-        mysqli_stmt_store_result($stmt2);
-        $result_check = mysqli_stmt_num_rows($stmt2);
-        if($result_check>0){
-          header("Location: register.php?error=2");
-          exit();
-        }
-      }
-
-      //insert query
-      $sqlq3 = "INSERT INTO `users` (`userName`, `email`, `password`) VALUES (?, ?, ?);";
-      $pswhash = passWord_hash($pass1, PASSWORD_DEFAULT);
-      $stmt3 = mysqli_stmt_init($conn);
-      if(!mysqli_stmt_prepare($stmt3, $sqlq3)){
-        echo 'SQL ERROR';
-      }else{
-        mysqli_stmt_bind_param($stmt3, "sss", $un, $email, $pswhash);
-        mysqli_stmt_execute($stmt3);
-        $sqlq4 = "INSERT INTO `reminder` (`uid`, `remText`) VALUES ((SELECT `id` from `users` where `userName` = ?),?)";
-        $stmt4 = mysqli_stmt_init($conn);
-        if(!mysqli_stmt_prepare($stmt4, $sqlq4)){
-          echo 'SQL ERROR';
-        }else{
-          mysqli_stmt_bind_param($stmt4, "ss", $un, $passrem);
-          mysqli_stmt_execute($stmt4);
-          header("Location: ../Welcome.php?signup=success");
-          exit();
+  function check_email($type, $id, $conn){
+    $stmt = mysqli_stmt_init($conn);
+    $sqlq = "SELECT * FROM `$type` WHERE `email` = ?";
+    if(!mysqli_stmt_prepare($stmt1, $sqlq1)){
+      echo 'SQL ERROR';
+    }else{
+      mysqli_stmt_bind_param($stmt, "s", $email);
+      mysqli_stmt_execute($stmt);
+      mysqli_stmt_store_result($stmt);
+      $result_check = mysqli_stmt_num_rows($stmt);
+      if($result_check>0){
+        if($type == "student"){
+          if($type != $id){
+            header("Location: ../signup.php?error=3");
+            exit();
+          }else{
+            header("Location: ../signup.php?error=1");
+            exit();
+          }
+        }else if($type == "teacher"){
+          if($type != $id){
+            header("Location: ../signup.php?error=4");
+            exit();
+          }else{
+            header("Location: ../signup.php?error=2");
+            exit();
+          }
         }
       }
     }
   }
+
+  function check_username($type, $id, $conn){
+    //userName check
+    $sqlq = "SELECT * FROM `$type` WHERE `username` = ?";
+    $stmt = mysqli_stmt_init($conn);
+    if(!mysqli_stmt_prepare($stmt2, $sqlq2)){
+      echo 'SQL ERROR';
+    }else{
+      mysqli_stmt_bind_param($stmt2, "s", $un);
+      mysqli_stmt_execute($stmt2);
+      mysqli_stmt_store_result($stmt2);
+      $result_check = mysqli_stmt_num_rows($stmt2);
+      if($result_check>0){
+        if($type == "student"){
+          if($type != $id){
+            header("Location: ../signup.php?error=7");
+            exit();
+          }else{
+            header("Location: ../signup.php?error=5");
+            exit();
+          }
+        }else if($type == "teacher"){
+          if($type != $id){
+            header("Location: ../signup.php?error=8");
+            exit();
+          }else{
+            header("Location: ../signup.php?error=6");
+            exit();
+          }
+        }
+      }
+    }
+  }
+
+  function insert($name, $surn, $email, $pass1, $un, $id, $conn){
+    //insert query
+    if($id == "teachers"){
+      $sqlq = "INSERT INTO `teachers_verify` (`name`, `surname`, `email`, `password`, `username`) VALUES (?, ?, ?, ?, ?);";
+      $message = "Please wait 24hours for an administrator to verify you are a teacher";
+    }else{
+      $sqlq = "INSERT INTO `$id` (`name`, `surname`, `email`, `password`, `username`) VALUES (?, ?, ?, ?, ?);";
+      $message = "success";
+    }
+    $pswhash = passWord_hash($pass1, PASSWORD_DEFAULT);
+    $stmt = mysqli_stmt_init($conn);
+    if(!mysqli_stmt_prepare($stmt, $sqlq)){
+      echo 'SQL ERROR';
+    }else{
+      mysqli_stmt_bind_param($stmt, "sssss", $name, $surn, $email, $pswhash, $un);
+      mysqli_stmt_execute($stmt);
+      header("Location: ../signup.php?signup=".$message);
+      exit();
+    }
+  }
+
 
  ?>
