023c2d8678b2d96cc5203f4e7d0e8626c61e442e
Doomy7 <g.xeirmpos@gmail.com>
2020-02-23 20:29:42 +0200

    Update: Admin accept registration application

--- prj/php/admin/action.php
+++ prj/php/admin/action.php
@@ -10,37 +10,17 @@
 
     $index = explode("_", key($_POST));
     if($_POST[key($_POST)] == "ACCEPT"){
-      echo($index[0]." ".$index[1]." ACCEPTED");
-      $name = $_SESSION[$index[0]][$index[1]]['name'];
-      $surname = $_SESSION[$index[0]][$index[1]]['surname'];
-      $email = $_SESSION[$index[0]][$index[1]]['email'];
-      $password = $_SESSION[$index[0]][$index[1]]['password'];
-      $username = $_SESSION[$index[0]][$index[1]]['username'];
+      copy_query($index, $conn);
+      delete($index, $conn, $_POST[key($_POST)]);
+      header("Location: verify_pending.php");
+      exit();
+
     }else if($_POST[key($_POST)] == "REJECT"){
-      echo($index[0]." ".$index[1]." REJECTED");
-      $name = $_SESSION[$index[0]][$index[1]]['name'];
-      $surname = $_SESSION[$index[0]][$index[1]]['surname'];
-      $email = $_SESSION[$index[0]][$index[1]]['email'];
-      $password = $_SESSION[$index[0]][$index[1]]['password'];
-      $username = $_SESSION[$index[0]][$index[1]]['username'];
-      $message = "Admin ".$_SESSION['name']." rejected ".$name." ".$surname." ".$email." ".$username." registration as ".$index[0]."!";
-      $log_time = date("Y/m/d") . ' ' . date("h:i:sa");
-      $stmt = mysqli_stmt_init($conn);
-      $type = $index[0]."_verify";
-      $sqlq = "DELETE FROM `$type` WHERE `vid` = ?;";
-      if(!mysqli_stmt_prepare($stmt, $sqlq)){
-        echo 'SQL ERROR';
-      }else{
-        mysqli_stmt_bind_param($stmt, "i", $index[1]);
-        mysqli_stmt_execute($stmt);
-      }
-      logit($conn, $message, $log_time);
+      delete($index, $conn, $_POST[key($_POST)]);
       header("Location: verify_pending.php");
       exit();
     }
 
-    #name	surname	email	password	username
-    #print_r($_SESSION[$index[0]][$index[1]]);
   }
 
   function logit($conn, $message, $log_time){
@@ -52,5 +32,52 @@
       mysqli_stmt_bind_param($stmt, "ss", $message, $log_time);
       mysqli_stmt_execute($stmt);
     }
+  }
+
+  function delete($index, $conn, $action){
+    $name = $_SESSION[$index[0]][$index[1]]['name'];
+    $surname = $_SESSION[$index[0]][$index[1]]['surname'];
+    $email = $_SESSION[$index[0]][$index[1]]['email'];
+    $password = $_SESSION[$index[0]][$index[1]]['password'];
+    $username = $_SESSION[$index[0]][$index[1]]['username'];
+    $stmt = mysqli_stmt_init($conn);
+    $type = $index[0]."_verify";
+    $sqlq = "DELETE FROM `$type` WHERE `vid` = ?;";
+    if(!mysqli_stmt_prepare($stmt, $sqlq)){
+      echo 'SQL ERROR';
+    }else{
+      mysqli_stmt_bind_param($stmt, "i", $index[1]);
+      mysqli_stmt_execute($stmt);
+    }
+    if($action == "ACCEPT"){
+      $message = $name." ".$surname." ".$email." ".$username." added as ".$index[0]."!";
+      $log_time = date("Y/m/d") . ' ' . date("h:i:sa");
+      logit($conn, $message, $log_time);
+    }else if($action == "REJECT"){
+      $message = "Admin ".$_SESSION['name']." rejected ".$name." ".$surname." ".$email." ".$username." registration as ".$index[0]."!";
+      $log_time = date("Y/m/d") . ' ' . date("h:i:sa");
+      logit($conn, $message, $log_time);
+    }
+
+  }
+
+  function copy_query($index, $conn){
+    $name = $_SESSION[$index[0]][$index[1]]['name'];
+    $surname = $_SESSION[$index[0]][$index[1]]['surname'];
+    $email = $_SESSION[$index[0]][$index[1]]['email'];
+    $password = $_SESSION[$index[0]][$index[1]]['password'];
+    $username = $_SESSION[$index[0]][$index[1]]['username'];
+    $stmt = mysqli_stmt_init($conn);
+    $type = $index[0]."_verify";
+    $sqlq = "INSERT INTO `$index[0]` (`name`, `surname`, `email`, `password`, `username`) SELECT `name`, `surname`, `email`, `password`, `username` from $type where `vid` = ?;";
+    if(!mysqli_stmt_prepare($stmt, $sqlq)){
+      echo 'SQL ERROR';
+    }else{
+      mysqli_stmt_bind_param($stmt, "i", $index[1]);
+      mysqli_stmt_execute($stmt);
+    }
+    $message = "Admin ".$_SESSION['name']." accepted ".$name." ".$surname." ".$email." ".$username." registration as ".$index[0]."!";
+    $log_time = date("Y/m/d") . ' ' . date("h:i:sa");
+    logit($conn, $message, $log_time);
   }
  ?>
